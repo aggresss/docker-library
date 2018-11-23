e2827f7b8948c8ceabc0dc639d19316819dc3c1e
aggresss <aggresss@163.com>
2018-11-23 17:20:44 +0800

    2018-11-23 17:20:44 CST W47D5

--- python/jupyter/docker-entrypoint.sh
+++ python/jupyter/docker-entrypoint.sh
@@ -3,61 +3,17 @@
 set -eo pipefail
 shopt -s nullglob
 
-definedCommand="bash"
 JUPYTER_NOTEBOOK_PATH="/root/jupyter-notebook"
 
-# if command starts with an option, prepend defined command
-if [ "${1:0:1}" = '-' ]; then
-    set -- ${definedCommand} "$@"
+if [ "${1}" = 'bash' ]; then
+    shift
+    set -- bash "$@"
+elif [ "${1}" = 'python' -o "${1}" = 'python3' ]; then
+    shift
+    set -- python3 "$@"
+else
+    set -- python3 "$@"
 fi
 
-# skip setup if they want an option for help
-wantHelp=
-for arg; do
-    case "$arg" in
-        -'?'|--help|--print-defaults|-V|--version)
-            wantHelp=1
-            break
-            ;;
-    esac
-done
-
-# usage: file_env VAR [DEFAULT]
-#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
-# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-file_env() {
-    local var="$1"
-    local fileVar="${var}_FILE"
-    local def="${2:-}"
-    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-        exit 1
-    fi
-    local val="$def"
-    if [ "${!var:-}" ]; then
-        val="${!var}"
-    elif [ "${!fileVar:-}" ]; then
-        val="$(< "${!fileVar}")"
-    fi
-    export "$var"="$val"
-    unset "$fileVar"
-}
-
-# usage: process_init_file FILENAME MYSQLCOMMAND...
-#    ie: process_init_file foo.sh mysql -uroot
-# (process a single initializer file, based on its extension. we define this
-# function here, so that initializer scripts (*.sh) can use the same logic,
-# potentially recursively, or override the logic used in subsequent calls)
-process_init_file() {
-    local f="$1"; shift
-    local definedCommand=( "$@" )
-
-    case "$f" in
-        *.sh)     echo "$0: running $f"; . "$f" ;;
-        *)        echo "$0: ignoring $f" ;;
-    esac
-    echo
-}
-
 exec "$@"
+
