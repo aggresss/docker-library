0238c3f0c3fbc168e63d1d32750ff32a8ad7bc6e
aggresss <aggresss@163.com>
2019-04-18 15:26:49 +0800

    2019-04-18 15:26:49 CST W15D4

--- elementary/autogen.sh
+++ elementary/autogen.sh
@@ -87,6 +87,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \\
         gawk \\
         net-tools \\
         iputils-ping \\
+        dnsutils \\
         netcat \\
         socat \\
         tcpdump \\
